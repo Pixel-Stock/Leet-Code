c07779c9b39bfcaf111c683797f14664e2dc1560
Pixel-Stock <mrugeshkulkarni17@gmail.com>
2026-07-09 18:43:28 +0530

    Time: 19 ms (17.39%), Space: 50.2 MB (16.3%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+public List<List<String>> groupAnagrams(String[] strs) {
+Map<String, List<String>> groups = new HashMap<>();
+
+    for (String s : strs) {
+        int[] freq = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        StringBuilder key = new StringBuilder();
+
+        for (int count : freq) {
+            key.append('#').append(count);
+        }
+
+        groups.computeIfAbsent(key.toString(), k -> new ArrayList<>()).add(s);
+    }
+
+    return new ArrayList<>(groups.values());
+}
+
+}
